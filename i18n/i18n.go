ca2e93925b0762a296264633568edff85eb9bbe1
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-08-21 12:40:30 +0200

    Fix Languages map to actually work.
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- i18n/i18n.go
+++ i18n/i18n.go
@@ -10,18 +10,21 @@ import (
 
 //go:embed locales/*.mo
 var localesFS embed.FS
-var Languages map[string]*gotext.Locale
+var Languages map[string]*gotext.Mo
 
 func init() {
 	files, err := localesFS.ReadDir("locales")
 	if err != nil {
 		panic(err)
 	}
-	Languages = make(map[string]*gotext.Locale)
+	Languages = make(map[string]*gotext.Mo)
 	for _, file := range files {
 		fileName := file.Name()
 		fileName = strings.TrimSuffix(fileName, ".mo")
-		Languages[fileName] = gotext.NewLocaleFS(fileName, localesFS)
+		lang := gotext.NewMoFS(localesFS)
+		lang.ParseFile("locales/" + fileName + ".mo")
+		Languages[fileName] = lang
+
 		fmt.Printf("fileName=%s\n", fileName)
 	}
 	fmt.Println("Langcode en, de:", Languages["en"].Get("languagecode"), Languages["de"].Get("languagecode"))
