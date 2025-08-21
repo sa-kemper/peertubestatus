d09265a30ebd0d261fb61ea28f3368e73fa2242a
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-08-21 04:08:41 +0200

    Make translation functions work.
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- cmd/Templater/TemplateReadIn.go
+++ cmd/Templater/TemplateReadIn.go
@@ -13,10 +13,25 @@ var Templates *template.Template
 func init() {
 	stat, err := os.Stat("templates")
 	if err == nil && stat.IsDir() {
-		Templates = template.Must(template.ParseGlob("TemplateOverride/*.gohtml"))
+		Templates = template.New("TestTemplate")
+		Templates.Funcs(template.FuncMap{
+			"translate": func(text string) string {
+				return ""
+			},
+		})
+		Templates, err = Templates.ParseGlob("TemplateOverride/*.gohtml")
+		template.Must(Templates, err)
+
 		return
 	}
-	Templates = template.Must(template.ParseFS(web.TemplateFilesFS, "*.gohtml"))
+	Templates = template.New("TestTemplate")
+	Templates = Templates.Funcs(template.FuncMap{
+		"translate": func(text string) string {
+			return ""
+		},
+	})
+	Templates, err = Templates.ParseFS(web.TemplateFilesFS, "*.gohtml")
+	template.Must(Templates, err)
 
 	if Templates == nil {
 		log.Fatal("Templates not found")
