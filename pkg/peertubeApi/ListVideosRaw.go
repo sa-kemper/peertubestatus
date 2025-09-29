194b54d5030f1daf1ad2eede56456d29bd676e2a
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-09-29 16:36:49 +0200

    Add ListVideosRaw.go that implements the /api/v1/videos peertube endpoint. This time with a Raw response. We do not trust that the peertube developers version their "rest api" properly
    Tested-by: samuel <196162023+sa-kemper@users.noreply.github.com>
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

---
+++ pkg/peertubeApi/ListVideosRaw.go
@@ -0,0 +1,46 @@
+package peertubeApi
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+var ErrorNoMoreResults = errors.New("no more results")
+
+func (api *ApiClient) ListVideosRaw(args ListVideosParams) (data []byte, err error) {
+
+	const endpoint = "videos"
+	_, err = ValidateVideoIncludeFlags(args.Include, api.isAdmin)
+	if err != nil {
+		return
+	}
+
+	var listVideosUrl = url.URL{
+		Scheme:     api.Protocol,
+		Host:       api.Host,
+		Path:       apiPrefix + endpoint,
+		ForceQuery: true,
+		RawQuery:   toQueryParams(args).Encode(),
+	}
+	listVideosUrl.Query().Add("host", api.Host)
+
+	httpResponse, err := api.doRequest(&http.Request{
+		Method: http.MethodGet,
+		URL:    &listVideosUrl,
+		Header: api.headers,
+		Host:   api.Host,
+	})
+	if err != nil {
+		return
+	}
+
+	defer httpResponse.Body.Close()
+	data, err = io.ReadAll(httpResponse.Body)
+	if len(data) < 25 {
+		return []byte{}, ErrorNoMoreResults
+	}
+	return
+
+}
