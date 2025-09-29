cd2d3e73d752e4564a8f78da129b2d566840fdda
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-09-29 16:27:28 +0200

    Load video databases (JSON files), and query for metadata
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

---
+++ pkg/StatsIO/VideoMetadata.go
@@ -0,0 +1,42 @@
+package StatsIO
+
+import (
+	"encoding/json"
+	"errors"
+	"os"
+	"path"
+
+	"github.com/sa-kemper/peertubestats/pkg/peertubeapi"
+)
+
+// VideoMetadata queries the "DB" for the metadata of the video and returns it.
+func VideoMetadata(id int64) (result peertubeapi.VideoData, err error) {
+	Database.dataMutex.RLock()
+	defer Database.dataMutex.RUnlock()
+	if Database.data == nil {
+		db, err := loadVideoDB()
+		if err != nil {
+			return result, err
+		}
+		Database.data = &db
+	}
+	result, ok := (*Database.data)[id]
+	if !ok {
+		return result, errors.New("requested video does not exist")
+	}
+	return result, nil
+}
+
+// loadVideoDB loads all metadata of every video ever seen.
+func loadVideoDB() (VideoDatabase, error) {
+	jsonDBBytes, err := os.ReadFile(path.Join(Database.DataFolder, "videoDB.json"))
+	if err != nil {
+		if os.IsNotExist(err) {
+			return make(map[int64]peertubeapi.VideoData), nil
+		}
+		return nil, err
+	}
+	var dbMap map[int64]peertubeapi.VideoData
+	err = json.Unmarshal(jsonDBBytes, &dbMap)
+	return dbMap, err
+}
