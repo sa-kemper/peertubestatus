09a727df939ddfc46f6259d45d78f56560aa5284
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-11-03 14:18:13 +0100

    Handle zero dates differently, this allows users to switch between timeframe views, without requesting a huge amount of data accidentally
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- web/templates/FrontPageRequest.go
+++ web/templates/FrontPageRequest.go
@@ -11,11 +11,12 @@ type FrontPageRequest struct {
 }
 
 func (fpr *FrontPageRequest) HandleZeroDate() {
-	now := time.Now()
-	if fpr.Dates.StartDate.IsZero() {
+	// now never contains the time as we do not care about it, only the date.
+	now := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), 0, 0, 0, 0, time.UTC)
+
+	// If both dates are zero, set them to now
+	if fpr.Dates.StartDate.IsZero() && fpr.Dates.EndDate.IsZero() {
 		fpr.Dates.StartDate = now
-	}
-	if fpr.Dates.EndDate.IsZero() {
 		fpr.Dates.EndDate = now
 	}
 
@@ -23,20 +24,26 @@ func (fpr *FrontPageRequest) HandleZeroDate() {
 	case "":
 		fallthrough
 	case "Daily":
-		if fpr.Dates.StartDate.Equal(now) {
+		// if no start date was specified, or the timeframe matches a previous calculation, reset it.
+		if fpr.Dates.StartDate.Equal(now) || fpr.Dates.StartDate.Equal(now.AddDate(0, -5, 0)) || fpr.Dates.StartDate.Equal(now.AddDate(-4, 0, 0)) {
 			fpr.Dates.StartDate = now.AddDate(0, 0, -6)
+			fpr.Dates.EndDate = now
 		}
 		// in case of a fallthrough assume daily
 		fpr.Timeframe = "Daily"
 
 	case "Monthly":
-		if fpr.Dates.StartDate.Equal(now) || fpr.Dates.EndDate.Sub(fpr.Dates.StartDate).Hours() < ((24*30)*2) {
+		// Reset to 5 months ago if start date is now or date range is too short
+		if fpr.Dates.StartDate.Equal(now) || fpr.Dates.EndDate.Sub(fpr.Dates.StartDate).Hours() < ((24*30)*2) || fpr.Dates.StartDate.Equal(now.AddDate(-4, 0, 0)) {
 			fpr.Dates.StartDate = now.AddDate(0, -5, 0)
-		}
-	case "Yearly":
-		if fpr.Dates.StartDate.Equal(now) || fpr.Dates.EndDate.Sub(fpr.Dates.StartDate).Hours() < ((24*30)*12)*2 {
-			fpr.Dates.StartDate = now.AddDate(-4, 0, 0)
+			fpr.Dates.EndDate = now
 		}
 
+	case "Yearly":
+		// Reset to 4 years ago if start date is now or date range is too short
+		if fpr.Dates.StartDate.Equal(now) || fpr.Dates.EndDate.Sub(fpr.Dates.StartDate).Hours() < ((24*30)*12)*2 {
+			fpr.Dates.StartDate = now.AddDate(-4, 0, 0)
+			fpr.Dates.EndDate = now
+		}
 	}
 }
