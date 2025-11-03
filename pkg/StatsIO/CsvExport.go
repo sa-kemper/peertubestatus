e18e05190d363345e3d20858912968d4f83c6b53
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-11-03 16:04:54 +0100

    Add a CsvGenerate function, which obtains and translates the video data from the database
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

---
+++ pkg/StatsIO/CsvExport.go
@@ -0,0 +1,62 @@
+package StatsIO
+
+import (
+	"flag"
+	"maps"
+	"strconv"
+
+	"github.com/sa-kemper/peertubestats/i18n"
+	"github.com/sa-kemper/peertubestats/internal/LogHelp"
+	"github.com/sa-kemper/peertubestats/pkg/peertubeApi"
+	"github.com/sa-kemper/peertubestats/web/templates"
+)
+
+type CsvGenerateParameters struct {
+	Videos          []peertubeApi.VideoData
+	DisplaySettings templates.FrontPageRequest
+	Scope           struct {
+		Views bool
+		Likes bool
+	}
+}
+
+func CsvGenerate(parameters CsvGenerateParameters) (csvData [][]string) {
+	csvData = make([][]string, len(parameters.Videos)+1)
+	var Mo, found = i18n.Languages[flag.Lookup("output-language").Value.String()]
+	var Translate = func(id string, vars ...interface{}) string { return id }
+
+	if !found {
+		LogHelp.NewLog(LogHelp.Warn, "cannot find requested language", map[string]interface{}{"availableLanguages": maps.Keys(i18n.Languages), "requestedLang": flag.Lookup("output-language").Value.String()}).Log()
+	} else {
+		Translate = Mo.Get
+	}
+
+	csvData[0] = []string{Translate("Video Name"), Translate("Video URL")}
+	for iterator, vid := range parameters.Videos {
+		iterator++
+		stats, err := ExportStats(vid.ID, parameters.DisplaySettings.Dates, parameters.DisplaySettings.Timeframe)
+		var statStringSlice []string
+		for _, stat := range stats {
+			statStringSlice = append(statStringSlice, strconv.Itoa(int(stat.Views.Data)))
+		}
+		if err != nil {
+			LogHelp.NewLog(LogHelp.Fatal, "cannot read stats for video", map[string]string{"error": err.Error()}).Log()
+		}
+
+		if iterator == 2 {
+			// complete header
+			for _, stat := range stats {
+				csvData[0] = append(csvData[0], stat.Time.Format("2006-01-02"))
+			}
+		}
+
+		csvData[iterator] = []string{
+			vid.Name,
+			"https://" + flag.Lookup("api-host").Value.String() + "/w/" + vid.ShortUUID,
+		}
+		// insert the stats data
+		csvData[iterator] = append(csvData[iterator], statStringSlice...)
+
+	}
+	return csvData
+}
