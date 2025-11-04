18b533732e13ffb6738efb3921d9107622900152
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-11-04 06:47:03 +0100

    Add API implementation for obtaining a thumbnail.
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

---
+++ pkg/peertubeApi/GetThumbnail.go
@@ -0,0 +1,43 @@
+package peertubeApi
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+func (api *ApiClient) GetThumbnail(id int64) (thumbnailData []byte, err error) {
+	var buffer bytes.Buffer
+	videoMetadata, err := api.GetVideoMetadata(strconv.FormatInt(id, 10))
+	if err != nil {
+		return
+	}
+	if videoMetadata.ThumbnailPath == "" {
+		return
+	}
+	endpointUrl := url.URL{
+		Scheme: api.Protocol,
+		Host:   api.Host,
+		Path:   videoMetadata.ThumbnailPath,
+	}
+	response, err := api.doRequest(
+		&http.Request{
+			Method: http.MethodGet,
+			URL:    &endpointUrl,
+			Proto:  api.Protocol,
+			Host:   api.Host,
+		})
+	if err != nil {
+		println("error connecting to", endpointUrl.String())
+		println(api.Host, api.Protocol, api.clientId)
+		println(err.Error())
+
+		return
+	}
+	defer response.Body.Close()
+
+	_, err = io.Copy(&buffer, response.Body)
+	return buffer.Bytes(), err
+}
