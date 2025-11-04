29a4493cc35a427270da0420cf0feb6bf29216b9
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-11-04 06:46:08 +0100

    Abstract the stored videos into a sync.map, this makes it straight forward to use it, and concurrently safe.
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- pkg/StatsIO/GetAllVideos.go
+++ pkg/StatsIO/GetAllVideos.go
@@ -1,26 +1,43 @@
 package StatsIO
 
 import (
+	"errors"
+	"sync"
+
 	"github.com/sa-kemper/peertubestats/pkg/peertubeApi"
 )
 
 func GetAllVideos() (Videos []peertubeApi.VideoData, err error) {
-	var VideoDB VideoDatabase
-	VideoDB, err = loadVideoDB()
-	if err != nil {
-		return nil, err
+	var VideoDB *sync.Map
+	if Database.data != nil {
+		VideoDB = Database.data
+	} else {
+		VideoDB, err = loadVideoDB()
+		if err != nil {
+			return nil, err
+		}
 	}
-	for _, video := range VideoDB {
-		Videos = append(Videos, video)
-	}
+	VideoDB.Range(
+		func(k, v interface{}) bool {
+			Videos = append(Videos, v.(peertubeApi.VideoData))
+			return true
+		})
 	return Videos, nil
 }
 
 func GetVideo(id int64) (video peertubeApi.VideoData, err error) {
-	var VideoDB VideoDatabase
-	VideoDB, err = loadVideoDB()
-	if err != nil {
-		return
+	var VideoDB *sync.Map
+	if Database.data != nil {
+		VideoDB = Database.data
+	} else {
+		VideoDB, err = loadVideoDB()
+		if err != nil {
+			return video, err
+		}
 	}
-	return VideoDB[id], nil
+	vid, exists := VideoDB.Load(id)
+	if !exists {
+		return video, errors.New("video not found")
+	}
+	return vid.(peertubeApi.VideoData), nil
 }
