6ca99d2afbbf231315c6eb100df338e42e47bc1f
samuel <196162023+sa-kemper@users.noreply.github.com>
2025-11-26 17:43:11 +0100

    Fixup template extraction tooling
    Signed-off-by: sa-kemper <196162023+sa-kemper@users.noreply.github.com>

--- tools/extractTemplateText.sh
+++ tools/extractTemplateText.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 grep -oP '(?<=translate ")[^"]*' "$1" | while read -r line; do
-    echo "msgid \"$line\"\r\nmsgstr \"$line\"\r\n" >> en.pot
+    printf "msgid \"$line\"\r\nmsgstr \"$line\"\r\n" >> en.pot
 done
